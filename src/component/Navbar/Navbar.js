a11cc5cb10f985e4a2d112cc0db0dd224f007f64
AJ Speller <ajspeller@gmail.com>
2019-05-11 04:51:28 -0400

    Setup sass and create Navbar component and styles

---
+++ src/component/Navbar/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './Navbar.scss';
+import logo from '../../logo.png';
+
+export default function Navbar() {
+  return (
+    <nav className='navbar'>
+      <img src={logo} alt='Logo' height='50' />
+      <ul className='nav-links'>
+        <li>
+          <a href='/' className='nav-link'>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href='/' className='nav-link'>
+            About
+          </a>
+        </li>
+        <li>
+          <a href='/' className='active nav-link'>
+            Tours
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
