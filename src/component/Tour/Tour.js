01ad65b52118e19495f3c4549d90cc5a8c559c20
AJ Speller <ajspeller@gmail.com>
2019-05-11 07:09:04 -0400

    Fix typo in styles and add ability to remove a tour

--- src/component/Tour/Tour.js
+++ src/component/Tour/Tour.js
@@ -2,14 +2,27 @@ import React, { Component } from 'react';
 import './Tour.scss';
 
 export default class Tour extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { showInfo: false };
+    this.clickHandler = this.clickHandler.bind(this);
+  }
+  clickHandler(e) {
+    this.setState({ showInfo: !this.state.showInfo });
+  }
   render() {
-    const { city, img, name, info } = this.props.tour;
-    console.log(this.props.tour);
+    const { id, city, img, name, info } = this.props.tour;
+    const { removeTour } = this.props;
     return (
       <article className='tour'>
         <div className='img-container'>
           <img src={img} alt='Paris' />
-          <span className='close-btn'>
+          <span
+            className='close-btn'
+            onClick={() => {
+              removeTour(id);
+            }}
+          >
             <i className='fas fa-window-close' />
           </span>
         </div>
@@ -17,11 +30,11 @@ export default class Tour extends Component {
           <h3>{city}</h3>
           <h4>{name}</h4>
           <h5>
-            <span>
+            <span onClick={this.clickHandler}>
               <i className='fas fa-caret-square-down' />
             </span>
           </h5>
-          <p>{info}</p>
+          {this.state.showInfo && <p>{info}</p>}
         </div>
       </article>
     );
--- src/component/TourList/index.js
+++ src/component/TourList/index.js
@@ -13,12 +13,17 @@ export default class TourList extends Component {
       tours: tourData
     };
   }
+  removeTour = (id) => {
+    const { tours } = this.state;
+    const filteredTours = tours.filter((tour) => tour.id !== id);
+    this.setState({ tours: filteredTours });
+  };
   render() {
     const { tours } = this.state;
     return (
       <section className='tourlist'>
         {tours.map((tour) => (
-          <Tour key={tour.id} tour={tour} />
+          <Tour key={tour.id} tour={tour} removeTour={this.removeTour} />
         ))}
       </section>
     );
